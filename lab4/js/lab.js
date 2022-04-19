70651b195d9ed519cfa6306894e344407384fe43
ThatAidanGuy <aidan6325@gmail.com>
2022-04-19 05:28:28 -0700

    Did lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,59 @@
+// Lab 4 for ART101
+// In this lab we write pseudocode to familiarize
+// ourselves with planning before writing a program.
+// Link: https://thataidanguy.github.io/art101/lab4/index.html
+// File: lab.js (This file contains JavaScript to run in
+// Lab 4's index.html.)
+// Written by Aidan Andreasen
+// on 4.19.2022
+
+
+//Pseudocoding a Sandwich
+//Take the ingredients out from where they are stored
+  //Search the pantry
+  //Search the fridge
+//Toast two slices of bread in the toaster
+  //Remember to take them out of the toaster
+//Spread mayonnaise on one slice of bread
+//Add the cheese to the sandwich
+  //Cut the slices of cheese
+  //Place them on the bread
+//Add the meat to the sandwich
+//Add the rest of the ingredients
+  //Add lettuce
+  //Add tomato
+//Place the final piece of bread on top to complete the sandwich
+
+
+//Remix Songs on HookTheory For Free (and Legally)
+//You will need:
+  //A web browser (such as Google Chrome)
+  //Screen recording software (such as QuickTime Player)
+  //Computer audio capture software (such as iShowU Audio Capture)
+  //Video editing software (such as iMovie)
+
+//Process
+//Find a webpage that links to HookTheory’s search page
+  //Note: You can’t go to HookTheory.com directly, as that page tries to sell you on the software for money.
+  //Example: https://www.hooktheory.com/blog/automatically-turn-a-major-song-into-minor-and-hear-how-it-sounds/
+//Type a song into the search bar
+//Click on the search result for the song you want
+//Click on any sections of the song you want to remix
+  //Note: If the song has multiple portions listed separately, open them in separate tabs by double clicking (on Mac) or control-clicking (on Windows) and selecting “Open in New Tab” from the dropdown menu that appears.
+//You may use the HookTheory editor to modify the song however you’d like. Press any buttons on the webpage (not including the browser UI buttons, like the Minimize or Back buttons) to alter the song’s key, tempo, instruments, etc. The options are practically limitless. However, you cannot save the audio files for free.
+//Go to System Preferences -> Sound
+//Change audio output to audio capture software
+  //Note: You will not be able to hear the sounds your computer makes while audio outputs here, so be sure to perfect your song remix beforehand
+//Open video recording software
+  //Note: If “Microphone” setting (or equivalent) is not set to video recording software, set it to that
+//Record new video
+//Press play button on HookTheory song remix, and let song play
+//Save video file
+//Repeat steps 9 through 11 for all portions of the song you have remixed
+//Change audio output back to speaker, headphones, or whichever default you had it set to before which you can actually hear
+//Open video file in video editing software
+//Drag in recordings of each song portion in the order you want them to play
+//Delete silence between song portions if you want to make them flow together as a single melody
+//Save edited video as audio file only (no video)
+//Wait an eternity for the file to render
+//Voila! Now you have an audio file of the song you remixed.
